437d5eef5201596415f5eeabf1f9444a8bd5c2ec
shreeyash1326 <113007418+shreeyash1326@users.noreply.github.com>
2022-09-12 14:36:17 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,14 @@
+class Employee
+{
+int id;
+String name;
+
+static String company="vedanta";
+static String address="Mumbai";
+
+ void displaydata()
+{
+
+System.out.println(id  + "  " +name );
+}
+}
---
+++ MainEmployee.java
@@ -0,0 +1,23 @@
+class MainEmployee
+{
+public static void main(String[] args)
+{
+Employee e1=new Employee();
+Employee e2=new Employee();
+Employee e3=new Employee();
+
+e1.id=1;
+e1.name="shreeyash";
+
+e2.id=2;
+e2.name="avinash";
+
+e3.id=1;
+e3.name="pawan";
+
+e1.displaydata();
+e2.displaydata();
+e3.displaydata();
+
+System.out.println();
+}}
